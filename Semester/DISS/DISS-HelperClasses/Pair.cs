d439043718e3eca21bdcdb7d227c1688fea900ee
TobMit <80719513+tobmit@users.noreply.github.com>
2024-02-28 22:14:11 +0100

    Pari class

---
+++ Semester/DISS/DISS-HelperClasses/Pair.cs
@@ -0,0 +1,13 @@
+﻿namespace DISS_HelperClasses;
+
+public class Pair<TFirst, TSecond>
+{
+    public TFirst First { get; set; }
+    public TSecond Second { get; set; }
+
+    public Pair(TFirst first, TSecond second)
+    {
+        First = first;
+        Second = second;
+    }
+}
