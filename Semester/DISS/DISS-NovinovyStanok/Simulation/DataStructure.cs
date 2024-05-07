f1b84a1eb5d0d5fabc4998645c59a4ef04b434c8
TobMit <80719513+tobmit@users.noreply.github.com>
2024-05-07 09:58:20 +0200

    Odstránené súbory pre odovzdávanie

--- Semester/DISS/DISS-NovinovyStanok/Simulation/DataStructure.cs
+++
@@ -1,6 +0,0 @@
-namespace DISS_NovinovyStanok.Simulation;
-
-public class DataStructure : EventArgs
-{
-    // nothing Here for now
-}
--- Semester/DISS/DISS-NovinovyStanok/Simulation/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace DISS_NovinovyStanok.Simulation;
-
-public class Person
-{
-    public int State { get; set; }
-    public double TimeOfArrival { get; set; }
-
-    public int ID { get; set; }
-}
